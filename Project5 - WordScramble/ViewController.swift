50142d650fe6d19518b87e514047719a482966f2
travisbrigman <travis1000@gmail.com>
2021-05-10 15:46:24 -0500

    Initial Commit

---
+++ Project5 - WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5 - WordScramble
+//
+//  Created by Travis Brigman on 5/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
